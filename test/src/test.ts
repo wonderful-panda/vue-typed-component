af944c4e16461ea82e36932de4e16d9f54bfb48a
Iwata Hidetaka <iwata0303@gmail.com>
2016-12-15 18:16:55 +0900

    setup karma test with chrome

---
+++ test/src/test.ts
@@ -0,0 +1,9 @@
+///<reference types="mocha" />
+import * as assert from "power-assert";
+import * as Vue from "vue";
+
+describe("vue-typesafe-component", function() {
+    it("power-assert works fine", function() {
+        assert(1 + 2 === 4);
+    });
+});
---
+++ test/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+    context: __dirname,
+    entry: "./src/test.ts",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "build.js"
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ["", ".ts", ".js"],
+        root: __dirname,
+        alias: {
+            vue: "vue/dist/vue.js"
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: "ts" },
+            { test: /\.json$/, loader: "json" },
+        ],
+        postLoaders: [
+            { test: /test\.ts$/, loader: "webpack-espower" },
+        ]
+    },
+};
